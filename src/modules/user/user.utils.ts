5a3ec4360624a02fadc662cd0a367cfb465b606b
noyonalways <noyonrahman2003@gmail.com>
2024-05-30 12:36:41 +0600

    fix: generateStudentId funtion

--- src/modules/user/user.utils.ts
+++ src/modules/user/user.utils.ts
@@ -16,11 +16,29 @@ const findLastStudentId = async () => {
     })
     .lean();
 
-  return lastStudent?.id ? lastStudent.id.substring(6) : undefined;
+  return lastStudent?.id ? lastStudent.id : undefined;
 };
 
 const generateStudentId = async (payload: TAcademicSemester) => {
-  const currentId = (await findLastStudentId()) || (0).toString();
+  // first time 0000
+
+  let currentId = (0).toString(); // by default 0000
+
+  const lastStudentId = await findLastStudentId(); // 2030 01 0001
+
+  const lastStudentYear = lastStudentId?.substring(0, 4);
+  const lastStudentSemesterCode = lastStudentId?.substring(4, 6);
+  const currentYear = payload.year;
+  const currentSemesterCode = payload.code;
+
+  if (
+    lastStudentId &&
+    lastStudentYear === currentYear &&
+    lastStudentSemesterCode === currentSemesterCode
+  ) {
+    currentId = lastStudentId.substring(6); // 0001
+  }
+
   let incrementalId = (Number(currentId) + 1).toString().padStart(4, "0");
   incrementalId = `${payload.year}${payload.code}${incrementalId}`;
   return incrementalId;
