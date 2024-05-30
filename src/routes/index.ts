31e6cdafdfbcf36b3b0d23e42097d7db267c6a02
noyonalways <noyonrahman2003@gmail.com>
2024-05-30 17:55:49 +0600

    feat(routes): acdemic faculty root routes added

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,4 +1,5 @@
 import { Router } from "express";
+import academicFacultyRoutes from "../modules/academic-faculty/academicFaculty.routes";
 import academicSemesterRoutes from "../modules/academic-semester/academicSemester.routes";
 import studentRoutes from "../modules/student/student.routes";
 import userRoutes from "../modules/user/user.routes";
@@ -17,6 +18,10 @@ const moduleRouts = [
   {
     path: "/academic-semesters",
     routes: academicSemesterRoutes,
+  },
+  {
+    path: "/academic-faculties",
+    routes: academicFacultyRoutes,
   },
 ];
 
