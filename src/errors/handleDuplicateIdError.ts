39f36835c4268861dbe36a31b76e4b1afd9c12e2
noyonalways <noyonrahman2003@gmail.com>
2024-06-02 23:39:59 +0600

    Handle erros:
    - zod validation error
    - mongoose validation error
    - mongoose cast error
    - mongoose duplicateId error
    - instance of AppError
    - instance of Error

---
+++ src/errors/handleDuplicateIdError.ts
@@ -0,0 +1,31 @@
+import { Error } from "mongoose";
+import { TErrorSources, TGenericErrorResponse } from "../interface";
+
+interface MongooseDuplicateKeyError extends Error {
+  code: number;
+  keyPattern: Record<string, unknown>;
+  keyValue: Record<string, unknown>;
+}
+
+const handleDuplicateIdError = (
+  err: MongooseDuplicateKeyError,
+): TGenericErrorResponse => {
+  const match = err?.message?.match(/"([^"]*)"/);
+  const extractedMessage = match && match[1];
+
+  const statusCode = 400;
+  const errorSources: TErrorSources = [
+    {
+      path: Object.keys(err?.keyPattern).toString(),
+      message: `${extractedMessage} is already exists`,
+    },
+  ];
+
+  return {
+    statusCode,
+    message: "Duplicate Id",
+    errorSources,
+  };
+};
+
+export default handleDuplicateIdError;
---
+++ src/interface/index.ts
@@ -0,0 +1,16 @@
+export type TErrorSources = {
+  path: string | number;
+  message: string;
+}[];
+
+export type TGenericErrorResponse = {
+  statusCode: number;
+  message: string;
+  errorSources: TErrorSources;
+};
+
+export type TCustomError = {
+  success?: boolean;
+  status?: number;
+  message: string;
+};
