5ff2e76f65ba492defee83f67a82b92d8b75a79e
noyonalways <noyonrahman2003@gmail.com>
2024-07-09 02:56:53 +0600

    update cors origin

--- src/app/app.ts
+++ src/app/app.ts
@@ -11,7 +11,16 @@ const app = express();
 app.use(morgan("dev"));
 app.use(express.json());
 app.use(cookieParser());
-app.use(cors({ origin: ["http://127.0.0.1:5500"] }));
+app.use(
+  cors({
+    origin: [
+      "http://127.0.0.1:5500",
+      "http://127.0.0.1:5173",
+      "http://localhost:5173",
+    ],
+    credentials: true
+  }),
+);
 app.use(express.urlencoded({ extended: true }));
 
 // application routes
