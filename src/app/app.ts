4b3d1da451c337d2039b0f8d829cc8a7dc7bfbdd
noyonalways <noyonrahman2003@gmail.com>
2024-07-11 13:10:29 +0600

    web-ui origin added

--- src/app/app.ts
+++ src/app/app.ts
@@ -17,8 +17,9 @@ app.use(
       "http://127.0.0.1:5500",
       "http://127.0.0.1:5173",
       "http://localhost:5173",
+      "https://phu-ui.noyonrahman.xyz",
     ],
-    credentials: true
+    credentials: true,
   }),
 );
 app.use(express.urlencoded({ extended: true }));
